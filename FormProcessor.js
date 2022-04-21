c1ca96f52a5442788ded7ff3dafb40ca52df8ccc
Christoph Thelen <note@qualyweb.com>
2022-04-21 14:48:24 +0200

    Update 1.0.7
    added option for confirmation dialog (js) before submitting the form if a confirmation text is set, added option for sandbox mode

---
+++ FormProcessor.js
@@ -0,0 +1,22 @@
+
+jQuery(document).ready(function() {
+	/*
+	// instantiate the WireTabs
+	$('.FormProcessor > .Inputfields').WireTabs({ // tabs will be prepended to #element
+	    items: $('.WireTab'), // items that it should tab (REQUIRED)
+	    rememberTabs: true, // whether it should remember current tab across requests
+	    skipRememberTabIDs: ['DeleteTab'], // array of tab IDs it should not remember between requests
+	    // id: 'PageEditTabs', // id attribute for generated tabbed navigation (optional)
+	    itemsParent: null, // parent element for items (better to omit when possible)
+	    cookieName: 'WireTab', // Name of cookie it uses to remember tabs
+	});
+	*/
+	// confirm dialog
+	$('.FormProcessor.InputfieldFormConfirm').submit( function() {
+		var msg = $(this).attr('data-confirm');
+		if(confirm(msg)) {
+			return true;
+		}
+		return false;
+	});
+});
